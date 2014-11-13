4244ed6c3c1ca5a5bc9427427b635748b2cea8f5
Rob Bayliss <rob@lastcallmedia.com>
2014-11-13 16:49:49 -0500

    Add classes

---
+++ DrushParam.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace lcm\lcmbuild;
+
+/**
+ * A Drush CLI parameter.
+ */
+class DrushParam {
+
+  /**
+   * @var string The parameter's value.
+   */
+  protected $value;
+
+  /**
+   * Set the parameter value from a text element.
+   *
+   * @param mixed $str
+   *   The value of the text element.
+   */
+  public function addText($str) {
+    $this->value = (string) $str;
+  }
+
+  /**
+   * Get the parameter's value.
+   *
+   * return string
+   *   The parameter value.
+   */
+  public function getValue() {
+    return $this->value;
+  }
+
+}
