542b06a845f3f667dc799551034bdf1f7b16c96f
Rob Bayliss <rob@lastcallmedia.com>
2014-11-24 09:42:18 -0500

    Add a target for setting up behat

---
+++ Resources/sample.FeatureContext.php
@@ -0,0 +1,10 @@
+<?php
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+class FeatureContext extends RawDrupalContext {
+
+  /**
+   * Your feature context.  Step code goes here.
+   */
+}
