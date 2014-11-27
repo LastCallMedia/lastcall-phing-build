1d0cb0979f9e173e661ff855519d7917641a561c
Rob Bayliss <rob@lastcallmedia.com>
2014-11-27 10:52:40 -0500

    Add test for ensuring we can run setup on an existing site

--- Tests/Common/CommonTest.php
+++ Tests/Common/CommonTest.php
@@ -15,11 +15,29 @@ class CommonTest extends \BuildFileTest {
   }
 
   public function testBehatSetup() {
-    $this->executeTarget('drupal-setup-behat');
+    $this->executeTarget('setup-behat');
     $dir = $this->getProject()->getProperty('project.dir.behat');
     $this->assertFileExists($dir . '/behat.local.yml');
     $this->assertFileExists($dir . '/behat.yml');
+    $this->assertFileExists($dir .'/bootstrap/FeatureContext.php');
     $this->assertStringEqualsFile($dir . '/.gitignore', 'behat.local.yml');
+  }
+
+  /**
+   * Assert that we can run setup against a directory without it
+   * destroying code we already have in place.
+   */
+  public function testBehatReSetup() {
+    $dir = $this->getProject()->getProperty('project.dir.behat');
+    file_put_contents($dir .'/.gitignore', 'test');
+    mkdir($dir . '/features');
+    mkdir($dir . '/bootstrap');
+    file_put_contents($dir .'/features/test.feature', 'test');
+    file_put_contents($dir .'/bootstrap/FeatureContext.php', 'test');
+    $this->executeTarget('setup-behat');
+    $this->assertStringEqualsFile($dir . '/.gitignore', 'test');
+    $this->assertStringEqualsFile($dir . '/features/test.feature', 'test');
+    $this->assertStringEqualsFile($dir . '/bootstrap/FeatureContext.php', 'test');
   }
 
 
