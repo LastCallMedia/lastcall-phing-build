5c8f5c44db157ce23108af04a702680413fdfd79
Rob Bayliss <rob@lastcallmedia.com>
2014-11-29 14:55:43 -0500

    Fix bootstrap to only work for a local install

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,4 @@
 <?php
-$vendor_dir = file_exists(__DIR__ . '/../vendor') ?
-  __DIR__ .'/../vendor' :
-  __DIR__ .'/../..';
 
-require_once $vendor_dir . '/autoload.php';
-require_once $vendor_dir . '/phing/phing/test/bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/phing/phing/test/bootstrap.php';
