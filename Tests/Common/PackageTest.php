75aea31dfbd6bc545a251b794cf9bddaeb6fb4ef
Rob Bayliss <rob@lastcallmedia.com>
2014-11-29 14:44:58 -0500

    Add composer package testing

---
+++ Tests/Common/PackageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace lastcall\Phing\Tests\Common;
+
+use Composer\IO\NullIO;
+use Composer\Util\ConfigValidator;
+
+class PackageTest extends \PHPUnit_Framework_TestCase {
+
+  function testComposerJSON() {
+    $io = new NullIO();
+    $validator = new ConfigValidator($io);
+
+    list($errors, $publishErrors, $warnings) = $validator->validate(__DIR__.'/../../composer.json');
+    foreach($errors as $error) {
+      $this->fail($error);
+    }
+    foreach($publishErrors as $error) {
+      $this->fail($error);
+    }
+    foreach($warnings as $warning) {
+      $this->fail($warning);
+    }
+  }
+}
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+$vendor_dir = file_exists(__DIR__ . '/../vendor') ?
+  __DIR__ .'/../vendor' :
+  __DIR__ .'/../..';
 
-require_once '../../autoload.php';
-require_once '../../phing/phing/test/bootstrap.php';
+require_once $vendor_dir . '/autoload.php';
+require_once $vendor_dir . '/phing/phing/test/bootstrap.php';
