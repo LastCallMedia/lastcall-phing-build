de4adfe0bf412e57955782c6e8c36a1a20065944
Rob Bayliss <rob@lastcallmedia.com>
2014-12-03 09:30:54 -0500

    Add project-setup.sh and helpful instructions in the README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+Last Call Media Phing Build System
+==================================
+
 [![Build Status](https://travis-ci.org/LastCallMedia/lastcall-phing-build.svg?branch=master)](https://travis-ci.org/LastCallMedia/lastcall-phing-build)
+
+The purpose of this project is to aid in the automation of testing and code quality tools for Last Call Media's web projects.  It is intended to be run either locally or in a CI environment, and it contains Phing tasks for some common operations.  [Phing documentation](http://www.phing.info/trac/wiki/Users/Documentation)
+
+Setup
+-----
+1. From the project root, run the following command: `curl https://raw.githubusercontent.com/LastCallMedia/lastcall-phing-build/master/project-setup.sh | sh`
+1. Edit the new build file that has been placed in build/build.xml, and change the parameters to fit your project.
+1. When you are ready to setup Behat testing, run `vendor/bin/phing setup:behat`.  This will setup a behat directory at the location you have specified in build.xml.
+1. Edit your behat.local.yml and set the base_url parameter to the correct URL for the local environment.
+1. Run vendor/bin/phing test:behat to run Behat tests.
+1. Run vendor/bin/phing ci-build to do a full "CI Build" as configured in your build.xml.
+
+Continuous Integration
+----------------------
+In CI environments, the ci-build target will be invoked with the following command:
+    cd build && composer install
+    vendor/bin/phing ci-build
+Whatever is inside your ci-build target will be run.  This means you can add steps to the build by adding tasks inside of the ci-build target.  Having a task "fail" inside of the ci-build target will fail the entire build, so your tasks should be set up to check return codes.  For example:
+    <drush command="updatedb" yes="true" checkreturn="true"/>
+In this task, setting "checkreturn" to true means that if this command does not complete successfully, it will fail the build.
+
+Tips
+----
+* You can add composer script aliases to the build/composer.json file.  For example, the following command will allow you to run `composer behat`:
+    "scripts": {
+        "behat": "vendor/bin/behat -c ../sites/all/behat/behat.yml"
+    }
+* You can add any composer libraries you'd like to the composer.json.  Commit the composer.json and the composer.lock to the repository, but not the vendor/ code.
+* This repository and the Last Call Behat repository will be updated frequently, so run composer update as often as you can.
---
+++ project-setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+test -f composer.json || { echo "{}" > composer.json; }
+composer config repositories.lastcall composer http://satis.lcmdev.com
+composer require lastcall/phing:~1.0 lastcall/behat:dev-master behat/mink-zombie-driver:~1.0
+
+cat << "EOF" > .gitignore
+vendor/
+artifacts/
+EOF
+
+# Do not overwrite build.xml...
+test -f build.xml && { exit 0; }
+cat << "EOF" > build.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<project name="project" default="ci-build">
+    <import file="vendor/lastcall/phing/common.xml"/>
+
+    <!-- PROPERTIES YOU CAN EDIT BELOW: -->
+    <!-- The path to the behat binary you wish to use. -->
+    <property name="behat.executable" value="vendor/bin/behat"/>
+    <!-- The path to your behat directory. -->
+    <property name="project.dir.behat" value="../sites/default/behat"/>
+    <!-- The path to your behat.yml (you probably don't need to change this) -->
+    <property name="behat.config" value="${project.dir.behat}/behat.yml"/>
+    <property name="project.dir.artifacts" value="./artifacts" override="true"/>
+    <!-- Paths to custom code.  -->
+    <!-- In the future we will use this for checking code style. -->
+    <!-- @see: http://www.phing.info/docs/guide/trunk/apds03.html-->
+    <fileset id="custom_code" dir="../sites/all">
+        <include name="modules/custom/**/*.php"/>
+        <include name="modules/custom/**/*.module"/>
+        <include name="modules/custom/**/*.inc"/>
+        <include name="modules/custom/**/*.info"/>
+        <include name="themes/**"/>
+
+        <!-- Exclude generated code: -->
+        <exclude name="**.views_default.inc"/>
+        <exclude name="**.strongarm.inc"/>
+        <exclude name="**.feeds_importer_default.inc"/>
+        <exclude name="**.field_group.inc"/>
+        <exclude name="**.context.inc"/>
+        <exclude name="**.features.inc"/>
+        <exclude name="**.features.*.inc"/>
+
+        <!-- Exclude non-code files-->
+        <exclude name="**/.sass-cache/**"/>
+        <exclude name="**.jpg"/>
+        <exclude name="**.png"/>
+        <exclude name="**.gif"/>
+        <exclude name="**.eot"/>
+        <exclude name="**.ttf"/>
+        <exclude name="**.woff"/>
+        <exclude name="**.svg"/>
+        <exclude name="**.txt"/>
+    </fileset>
+
+    <target name="ci-build" description="Steps to be run in a CI environment">
+        <drush command="updatedb" yes="true" checkreturn="true"/>
+        <drush command="features-revert-all" yes="true" checkreturn="true"/>
+        <!-- Optional: Enable testing for the site.
+        <phingcall target="test:behat"/>-->
+    </target>
+</project>
+
+EOF
